81ace74950aad98c3ba08ae5a7dd74ee0a093cb0
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-24 23:35:03 +0530

    Added: Line comparision

---
+++ LineComparisionProblem/LineComparisionProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Use case1:");
+            UC1CalculateLength obj1 = new UC1CalculateLength();
+            obj1.calculatecoridinates();*/
+
+/*            Console.WriteLine("Use case2:");
+            UC2ToCheckEquality obj2 = new UC2ToCheckEquality();
+            obj2.ToCheckEquality();*/
+
+            Console.WriteLine("Use case3:");
+            UC3CompareToMethod obj3 = new UC3CompareToMethod();
+            obj3.CompareToMethod();
+        }
+    }
+}
---
+++ LineComparisionProblem/LineComparisionProblem/UC3CompareToMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionProblem
+{
+   public  class UC3CompareToMethod
+    {
+        public void CompareToMethod()
+        { 
+        Console.WriteLine("Welcome to Line Comparison Computation");
+         Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
+         int x1 = Convert.ToInt32(Console.ReadLine());
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        int y2 = Convert.ToInt32(Console.ReadLine());
+        int length1 = toFindLength(x1, y1, x2, y2);
+
+        Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
+        x1 = Convert.ToInt32(Console.ReadLine());
+        x2 = Convert.ToInt32(Console.ReadLine());
+         y1 = Convert.ToInt32(Console.ReadLine());
+         y2 = Convert.ToInt32(Console.ReadLine());
+          int length2 = toFindLength(x1, y1, x2, y2);
+
+        Console.WriteLine(length1.Equals(length2));
+
+         Console.WriteLine("Return value (comparison) =" + length1.CompareTo(length2));
+
+        }
+    private static int toFindLength(int x1, int y1, int x2, int y2)
+    {
+        return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    }
+}
+}
